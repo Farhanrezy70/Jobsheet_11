5c36d8f49d1f96063256e2ed802610f4d6ee8d6a
Farhanrezy <farhanrezy70@gmail.com>
2023-11-09 08:57:48 +0700

    Percobaan 2

---
+++ Square.java
@@ -0,0 +1,20 @@
+package Jobsheet_11;
+import java.util.Scanner;
+
+public class Square {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = input.nextInt();
+        for(int iOuter = 1; iOuter <= N; iOuter++){
+            for(int i = 0; i <= N; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
