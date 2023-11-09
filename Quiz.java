af57a44a0bcc8d18a949a6a19dfb0f006fa6a390
Farhanrezy <farhanrezy70@gmail.com>
2023-11-09 09:54:48 +0700

    Percobaan 4

---
+++ Quiz.java
@@ -0,0 +1,34 @@
+package Jobsheet_11;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+         
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        char menu = 'y';
+        do{
+            int number = random.nextInt(10)+1;
+            boolean success = false;
+            do{
+                System.out.print("tebak angka (1-10) : ");
+                int answer = input.nextInt();
+                input.nextLine();
+                    if (answer < number){
+                        System.out.println("Angka yang anda masukkan lebih kecil dari jawaban.");
+                    }else if (answer > number){
+                        System.out.println("Angka yang anda masukkan lebih besar dari jawaban");
+                    }else {
+                        System.out.println("Jawaban Anda benar!!");
+                        success = true;
+                    }
+            }while (!success);
+                System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
+                menu = input.nextLine().charAt(0);
+                    }while (menu == 'y' || menu == 'Y');
+    }
+    
+}
